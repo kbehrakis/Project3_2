8bc96962b04f352d1ffc809e6af2a95bdd111213
Kristen Behrakis <kbehrakis@olin.edu>
2016-11-14 17:09:28 -0500

    Can now calculate acceleration based on given applied and pendulum force

--- torque.m
+++ torque.m
@@ -1,1 +1,21 @@
 %Hello World
+
+% Set varaibles
+pendulumLength = 5; 
+pendulumForce = 50; % Will need to call to equation to get actual pendulum force
+buildingHeight = 200; 
+buildingBase = 100;
+buildingMass = 10000;
+appliedForce = 100;
+
+% Torque due applied force
+torqueApplied = buildingHeight * appliedForce;
+
+% Torque due to pendulum force 
+torquePendulum = pendulumLength * pendulumForce; 
+
+% Sum of torques
+sumTorques = torquePendulum - torqueApplied;
+
+% Translational acceleration
+acceleration = 12*(sumTorques)/((buildingBase^2 + buildingBase^2) * buildingMass)
