2f530d13aae14e7e67a3ba312a4e0ff698a05d98
kbehrakis <kbehrakis@olin.edu>
2016-11-23 17:05:02 -0500

    Displacement of building.  Includes correct damping (c) values

--- buildingForce.m
+++ buildingForce.m
@@ -5,16 +5,24 @@
 function res = buildingForce()
     % Start conditions
     appliedForce = sin(pi/4);   % Force of earthquake, CHANGE (RESEARCH)
-    buildingStiffness = 20;    % Stiffness constant of building, MUST CHANGE (RESEARCH)
+    buildingStiffness = 200;    % Stiffness constant of building, MUST CHANGE (RESEARCH)
     heightOfBuilding = 100;     % Height of skyscraper (m)
     massOfBuilding = 201848605; % Mass of skyscraper (kg)
-    dampingConstant = 9000;     % MUST CHANGE, PROBABLY CALL TO PENDULUM FUNCTION TO FIND APPROPRIATE RATIO
     velocityStart = 0;          % Velocity at top, starts at 0
     xStart = 0;                 % Start distance without force
-  
+   
+    % Damping coeffecient: c = zeta * (2* sqrt(mass*buildingStiffness), where 
+    %                      zeta = 1, critical damping
+    %                      0 < zeta < 1, underdamped 
+    %                      zeta > 1, overdamped
+    % https://en.wikipedia.org/wiki/Harmonic_oscillator#Example:_mass-spring-damper
+    % https://www.et.byu.edu/~tom/classes/436/ClassNotes/Class20(Second-Order).pdf)
+    zeta = 0.4;
+    dampingConstant = zeta*2*sqrt(buildingStiffness*massOfBuilding);
+   
     % ode45
     startTime = 0;
-    endTime = 86400; % 1 day
+    endTime = 86400/4; % 6 hours
     parameters = [xStart, velocityStart];
     [Times, UpdatedParameters] = ode45(@moveBuilding, [startTime, endTime], parameters);
    
@@ -27,10 +35,12 @@ function res = buildingForce()
    xlabel('Time (s)');
    ylabel('Displacement (m)');
    title('Horizontal Displacement of Top of Building')
+   
+   % Display which zeta value is being used
+   text(Times(18),Displacement(18),['\zeta = ' num2str(zeta) '']);
     
     % Update equations of motion
      function res = moveBuilding(~, CurrentValues)
-         
          % Unpack the vector
          % Get the current displacement
          currentX = CurrentValues(1);
@@ -49,7 +59,7 @@ function res = buildingForce()
          currentForce = appliedForce - buildingElasticity - dampingForce;
           
          % Find the acceleration at top of building
-         currentAcceleration = currentForce/massOfBuilding; % NEED TO CHANGE MASS TO ADD IN PENDULUM MASS
+         currentAcceleration = currentForce/massOfBuilding; % PROBABLY NEED TO CHANGE MASS TO ADD IN PENDULUM MASS
          
          % Return the results, keep in mind ode45 will intergrate
          res = [currentV; currentAcceleration];
